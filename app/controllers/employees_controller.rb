a859575389388844decb3e70d01bfc5b910c9dd8
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-10 11:47:44 +0100

    test

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,27 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+        respond_to do |format|
+        format.html
+        format.json {render json: @employees.as_json(except: [:created_at, :updated_at], root: true, include: {department: {except: [:created_at, :updated_at]}})}
+        format.xml {render xml: @employees.as_json(except: [:created_at, :updated_at], root: true, include: {department: {except: [:created_at, :updated_at]}})}
+        end
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        respond_to do |format|
+        format.html
+        format.json {render json: @employee.as_json }
+        format.xml {render xml: @employee.as_json}
+        end
+    end
+
+    private
+
+	def employee_params
+		params.require(:employee).permit!
+	end
+
 end
+
---
+++ app/views/departments/show.xml.builder
@@ -0,0 +1,6 @@
+xml.instruct!
+xml.employee do
+  xml.name @employee.name 
+  xml.rank (@employee.rank, type: 'integer')
+  xml.department @employee.department.name
+end
